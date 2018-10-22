fc3eeaace69a9791dabf1dbe1d88a0b7d65d4b56
angel <angelblas17@gmail.com>
2018-10-22 16:48:17 -0700

    fixed images

--- js/app.js
+++ js/app.js
@@ -1,36 +1,52 @@
-    var allItems=[];
-    
-    
-    
-    function item(name, filepath)
-    this.name= name;
-    this.filepath= filepath;
-    allItems.push(this);
+'use strict';
 
-
-    new item("bag", 'img/bag.jpg');
-    new item("banana", 'img/banana.jpg');
-    new item("bathroom", 'img/bathroom.jpg');
-    new item("boots", 'img/boots.jpg');
-    new item("breakfast", 'img/breakfast.jpg');
-    new item("bubblegum", 'img/bubblegum.jpg');
-    new item("chair", 'img/chair.jpg');
-    new item("cthulhu", 'img/cthulhu.jpg');
-    new item("dog-duck", 'img/dog-duck.jpg');
-    new item("dragon", 'img/dragon.jpg');
-    new item("pen", 'img/pen.jpg');
-    new item("pet-sweep", 'img/pet-sweep.jpg');
-    new item("scissors", 'img/scissors.jpg');
-    new item("shark", 'img/shark.jpg');
-    new item("sweep", 'img/sweep.jpg');
-    new item("tauntaun", 'img(/tauntaun.jpg');
-    new item("unicorn", 'img/unicorn.jpg');
-    new item("usb", 'img/usb.jpg');
-    new item("water-can", 'img/watercan.jpg');
-    new item("wine-glass", 'img/wine-glass.jpg');
+var allItems = [];
 
 
 
+function Item(name, filepath) {
+  this.name = name;
+  this.filepath = filepath;
+  allItems.push(this);
+}
+
+new Item('bag', 'img/bag.jpg');
+new Item('banana', 'img/banana.jpg');
+new Item('bathroom', 'img/bathroom.jpg');
+new Item('boots', 'img/boots.jpg');
+new Item('breakfast', 'img/breakfast.jpg');
+new Item('bubblegum', 'img/bubblegum.jpg');
+new Item('chair', 'img/chair.jpg');
+new Item('cthulhu', 'img/cthulhu.jpg');
+new Item('dog-duck', 'img/dog-duck.jpg');
+new Item('dragon', 'img/dragon.jpg');
+new Item('pen', 'img/pen.jpg');
+new Item('pet-sweep', 'img/pet-sweep.jpg');
+new Item('scissors', 'img/scissors.jpg');
+new Item('shark', 'img/shark.jpg');
+new Item('sweep', 'img/sweep.jpg');
+new Item('tauntaun', 'img(/tauntaun.jpg');
+new Item('unicorn', 'img/unicorn.jpg');
+new Item('usb', 'img/usb.jpg');
+new Item('water-can', 'img/watercan.jpg');
+new Item('wine-glass', 'img/wine-glass.jpg');
+
+
+var itemImage = document.getElementById('item-pic');
+
+function randomItem() {
+  var idx = Math.floor(Math.random() * allItems.length);
+
+  itemImage.src = allItems[idx].filepath;
+  itemImage.alt = allItems[idx].name;
+  itemImage.title = allItems[idx].name;
+
+  allItems[idx].views++;
+}
+
+randomItem();
+
+itemImage.addEventListener('click', randomItem);
 
 
 
