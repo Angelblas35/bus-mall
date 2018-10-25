203967d4dcdc1d17e4dc3008a722263b4f19d66c
angel <angelblas17@gmail.com>
2018-10-24 17:56:24 -0700

    got the chart to populate

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 
 var allItems = [];
 var votes = [];
-// var title = [];
+
 
 var container = document.getElementById('image-container');
 var left = document.getElementById('left');
@@ -11,22 +11,24 @@ var right = document.getElementById('right');
 var itemList = document.getElementById('itemlist');
 
 
-function Item(name, votes, timeShown) {
+function Item(name) {
   this.name = name;
   this.votes = votes || 0;
-  this.timeShown = timeShown || 0;
+  // this.timeShown = timeShown || 0;
   this.filepath = `img/${name}.jpg`;
   allItems.push(this);
 }
 
-var allProducts = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthuluhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water', 'wine-glass'];
+var allProducts = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
 
 
 
 
-allProducts.forEach (function (productItem){
-  new Item (productItem);
-});
+allProducts.forEach (
+  function (productItem){
+    new Item (productItem);
+  }
+);
 
 // for (var i = 0; i < allProducts.length; i++) {
 //   new Item(allProducts[i]);
@@ -43,19 +45,11 @@ function endClicks() {
     container.removeEventListener('click', handleClick);
     // build votes array
     // render chart or list
-    showlist();
+    drawChart();
 
   }
 }
 
-
-
-
-// endClicks();
-
-// function makeRandom() {
-//   return Math.floor(Math.random() * allItems.length);
-// }
 
 var output = [];
 function makeThreeUnique() {
@@ -106,10 +100,11 @@ function handleClick(event) {
 
 
 
-  console.log(event.target.alt);
+  // console.log(event.target.alt);
   for (var i = 0; i < allItems.length; i++) {
     if (event.target.title === allItems[i].name) {
       allItems[i].votes++;
+      
     }
   }
   makeThreeUnique();
@@ -118,92 +113,29 @@ function handleClick(event) {
 
 
 
-function showlist() {
+function showChart() {
   for (var i = 0; i < allItems.length; i++) {
     var liEl = document.createElement('li');
-    liEl.textContent = `${allItems[i].name} has ${allItems[i].views}
+    liEl.textContent = `${allItems[i].name} has ${allItems[i].timeShown}
     and ${allItems[i].votes} votes`;
     itemList.appendChild(liEl);
   }
 }
-// var canvas = document.getElementById('canvas');
-// if (canvas.getContext) {
-//   var chart = canvas.getContext('2d');
-// }
 
 container.addEventListener('click', handleClick);
 makeThreeUnique();
 
 
-// var allProducts = [];
+
 var busChart;
 var chartDrawn = false;
 
-// ++++++++++++++++++++++++++++++++++++++++++++
-// DATA - Constructor and instances
-// ++++++++++++++++++++++++++++++++++++++++++++
 
-// function Item(title, identifier) {
-//   this.title = title;
-//   this.votes = 0;
-//   this.identifier = identifier;
-//   allProducts.push(this);
-// }
-
-new Item('Bag', 'bag');
-new Item('Banana', 'banana');
-new Item('Bathroom', 'bathroom');
-new Item('Boots', 'boots');
-new Item('Breakfast', 'breakfast');
-new Item('Bubblegum', 'bubblegum');
-new Item('Chair', 'chair');
-new Item('Cthulhu', 'cthulhu');
-new Item('Dog-Duck', 'dog-duck');
-new Item('Dragon', 'dragon');
-new Item('Pen', 'pen');
-new Item('Pet-Sweep', 'pet-sweep');
-new Item('Scissors', 'scissors');
-new Item('Shark', 'shark');
-new Item('Sweep', 'sweep');
-new Item('Tauntaun', 'tauntaun');
-new Item('Unicorn', 'unicorn');
-new Item('Usb', 'usb');
-new Item('Water-Can', 'water-can');
-new Item('Wine-Glass', 'wine-glass');
-
-// Arrays to hold data for the chart
-// var votes = [];
 var titles = [];
-
-// ++++++++++++++++++++++++++++++++++++++++++++
-// FUNCTION DECLARATIONS
-// ++++++++++++++++++++++++++++++++++++++++++++
-
 function updateChartArrays() {
-  for (var i = 0; i < allProducts.length; i++) {
-    titles[i] = allProducts[i].title;
-    votes[i] = allProducts[i].votes;
-  }
-}
-
-function showProductsAsList() {
-  var itemList = document.getElementById('mellow-list');
-  itemList.innerHTML = '';
-  itemList.hidden = false;
-  itemList.textContent = 'CLICK ON THIS LIST TO HIDE IT';
-  for (var i = 0; i < allProducts.length; i++){
-    var liEl = document.createElement('li');
-    liEl.textContent = allProducts[i].title + ', ' + allProducts[i].votes + ' votes';
-    itemList.appendChild(liEl);
-  };
-};
-
-function tallyVote(thisSong) {
-  for (var i = 0; i < allProducts.length; i++) {
-    if (thisSong === allProducts[i].identifier) {
-      allProducts[i].votes++;
-      updateChartArrays();
-    }
+  for (var i = 0; i < allItems.length; i++) {
+    titles[i] = allItems[i].name;
+    votes[i] = allItems[i].votes;
   }
 }
 
@@ -218,23 +150,24 @@ var data = {
         'bisque',
         'darkgray',
         'burlywood',
-        'lightblue',
+        'darkgreen',
         'navy'
       ],
       hoverBackgroundColor: [
-        'darkgreen',
-        'darkgreen',
-        'darkgreen',
-        'darkgreen',
-        'darkgreen'
+        'grey',
+        'grey',
+        'grey',
+        'grey',
+        'grey'
       ],
     }],
 };
 
 function drawChart() {
-  var ctx = document.getElementById('mellow-chart').getContext('2d');
+  updateChartArrays();
+  var ctx = document.getElementById('bus-chart').getContext('2d');
   busChart = new Chart(ctx,{
-    type: 'polarArea',
+    type: 'bar',
     data: data,
     options: {
       legend: {
@@ -263,35 +196,13 @@ function drawChart() {
 }
 
 function hideChart() {
-  document.getElementById('mellow-chart').hidden = true;
+  document.getElementById('bar-chart').hidden = true;
 }
-// ++++++++++++++++++++++++++++++++++++++++++++
-// EVENT LISTENERS
-// ++++++++++++++++++++++++++++++++++++++++++++
 
-document.getElementById('draw-chart').addEventListener('click', function(){
-  drawChart();
-  // setTimeout(hideChart, 5000);
-});
 
-document.getElementById('list-button').addEventListener('click', function(){
-  showProductsAsList();
-});
+if (chartDrawn) {
+  busChart.update();
+}
 
-// document.getElementById('list-button').addEventListener('click', showSongsAsList);
-
-document.getElementById('mellow-list').addEventListener('click', function(){
-  document.getElementById('mellow-list').hidden = true;
-});
-
-document.getElementById('voting').addEventListener('click', function(event){
-  if (event.target.id !== 'voting') {
-    tallyVote(event.target.id);
-  };
-
-  if (chartDrawn) {
-    busChart.update();
-  }
-});
 
 
